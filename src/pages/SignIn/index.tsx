df18fbcb3de4d7fc9aca787c7ec5454968330a1d
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-11-12 20:19:22 -0300

    estilizando tela de SingIn

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Image } from 'react-native';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import logoImg from '../../assets/logo.png';
 import { Container, Title } from './styles';
 
-import logoImg from '../../assets/logo.png'
-
 const SignIn: React.FC = () => {
-  return <Container>
-    <Image source={logoImg} />
-    <Title>Faça seu Logon</Title>
-  </Container>;
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu Logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => {}}>Entrar</Button>
+    </Container>
+  );
 };
 
 export default SignIn;
